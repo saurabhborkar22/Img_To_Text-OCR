3414d0df937620d3b53e3c00d04bbd64ba0fad51
saurabh suresh borkar <117628848+saurabhborkar22@users.noreply.github.com>
2023-04-13 02:22:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+import pytesseract
+import cv2
+import numpy as np
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['image']
+    img = cv2.imdecode(np.fromstring(file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+    text = pytesseract.image_to_string(img)
+    return render_template('result.html', text=text)
+
+if __name__ == '__main__':
+    app.run(debug=True)
